2e9dd67c239f4595175c67e2f835813d02030b69
ClassicIsBetter <dexterbooth3@proton.me>
2026-08-07 20:52:18 +0930

    Create supabase.js

---
+++ database/supabase.js
@@ -0,0 +1,22 @@
+```js
+const ws = require("ws");
+const { createClient } = require("@supabase/supabase-js");
+
+const config = require("../config/config");
+
+const supabase = createClient(
+  config.SUPABASE_URL,
+  config.SUPABASE_KEY,
+  {
+    auth: {
+      persistSession: false
+    },
+
+    realtime: {
+      transport: ws
+    }
+  }
+);
+
+module.exports = supabase;
+```
