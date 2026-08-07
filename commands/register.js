1c840f0f20b4452bbc61ae3417618019689c28e1
ClassicIsBetter <dexterbooth3@proton.me>
2026-08-07 21:02:12 +0930

    Create register.js

---
+++ commands/register.js
@@ -0,0 +1,36 @@
+```js
+const { REST, Routes } = require("discord.js");
+const config = require("../config/config");
+const commands = require("./definitions");
+
+async function registerCommands() {
+
+  const rest = new REST({
+    version: "10"
+  }).setToken(config.TOKEN);
+
+  try {
+
+    console.log("Registering commands...");
+
+    await rest.put(
+      Routes.applicationCommands(config.CLIENT_ID),
+      {
+        body: commands
+      }
+    );
+
+    console.log("Commands ready.");
+
+  } catch (error) {
+
+    console.error(
+      "Failed to register commands:",
+      error
+    );
+
+  }
+}
+
+module.exports = registerCommands;
+```
